0b6f0b158bad219a38aad0454be7040bf88557c0
Vic-Dev <vmichellej@gmail.com>
2015-12-19 17:57:15 -0500

    Add items, button and text style, and buy/info button functionality

--- crappy-clicker.js
+++ crappy-clicker.js
@@ -1,12 +1,37 @@
 var scoreElement = document.getElementById("score-num")
 var scoreNum = 0;
+var strongFinger = 0;
+var currentScore = 0;
 
 function newGame() {
 	scoreNum = 0;
 	scoreElement.innerHTML = "0";
+	currentScore = 0;
+	strongFinger = 0;
 }
 
 function clickMe() {
-	scoreNum++;
-	scoreElement.innerHTML = scoreNum;
+	scoreNum = scoreNum + 1 + strongFinger;
+	scoreElement.innerHTML = currentScore + scoreNum;
+}
+
+function buyStrongFinger() {
+	if (scoreElement.innerHTML >= 10) {
+		scoreNum = 0;
+		scoreElement.innerHTML = scoreElement.innerHTML - 10;
+		currentScore = Number(scoreElement.innerHTML);
+		strongFinger = 1;
+	};
+}
+
+function infoStrongFinger() {
+	alert("you clicked info for strong finger");
+}
+
+function buyAutoClicker() {
+	alert("you clicked buy for auto clicker");
+}
+
+function infoAutoClicker() {
+	alert("you clicked info for auto clicker");
 }
