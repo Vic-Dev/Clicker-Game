84431bf2d548919b88cd08ffa24e9074e15cb13f
Vic-Dev <vmichellej@gmail.com>
2015-12-28 18:55:43 -0500

    Add Info and Current/Next Level

--- crappy-clicker.js
+++ crappy-clicker.js
@@ -3,7 +3,16 @@ var scoreNum = 0;
 var strongFinger = 0;
 var autoClicker = [];
 var MAXAUTOCLICKER = 10;
-var autoClickerLevelElement = document.getElementById("auto-clicker-level");
+var autoClickerLevelElement = document.getElementById("next-auto-clicker-level");
+var strongFingerLevelElement = document.getElementById("next-strong-finger-level");
+var strongFingerLevel = 0;
+var infoStrongFingerText = "";
+var strongFingerNextLevel = 0;
+var infoAutoClickerText = "";
+var autoClickerLevel = 0;
+var autoClickerNextLevel = 0;
+var currentStrongFingerLevelElement = document.getElementById("current-strong-finger-level");
+var currentAutoClickerLevelElement = document.getElementById("current-auto-clicker-level");
 
 function newGame() {
 	scoreNum = 0;
@@ -14,6 +23,13 @@ function newGame() {
 	}
 	autoClicker = [];
 	autoClickerLevelElement.innerHTML = 1;
+	strongFingerLevelElement.innerHTML = 1;
+	currentStrongFingerLevelElement.innerHTML = 0;
+	currentAutoClickerLevelElement.innerHTML = 0;
+	strongFingerLevel = 0;
+	strongFingerNextLevel = 0;
+	autoClickerLevel = 0;
+	autoClickerNextLevel = 0;
 }
 
 function clickMe() {
@@ -26,11 +42,17 @@ function buyStrongFinger() {
 		scoreNum -= 10;
 		scoreElement.innerHTML = scoreNum;
 		strongFinger = 1;
+		strongFingerLevel++;
+		strongFingerNextLevel = strongFingerLevel + 1;
+		strongFingerLevelElement.innerHTML = strongFingerNextLevel;
+		currentStrongFingerLevelElement.innerHTML = strongFingerLevel;
 	}
 }
 
 function infoStrongFinger() {
-	alert("you clicked info for strong finger");
+	strongFingerNextLevel = strongFingerLevel + 1;
+	infoStrongFingerText = "Make your finger STRONGER!!~ Current Level " + strongFingerLevel + ", Next Level " + strongFingerNextLevel;
+	alert(infoStrongFingerText);
 }
 
 function buyAutoClicker() {
@@ -38,7 +60,10 @@ function buyAutoClicker() {
 		scoreNum -= 10;
 		scoreElement.innerHTML = scoreNum;
 		autoClicker.push(window.setInterval(autoClickerFunc, 1000));
-		autoClickerLevelElement.innerHTML = autoClicker.length + 1;
+		autoClickerLevel = autoClicker.length
+		autoClickerNextLevel = autoClickerLevel + 1
+		autoClickerLevelElement.innerHTML = autoClickerNextLevel;
+		currentAutoClickerLevelElement.innerHTML = autoClickerLevel;
 	}
 }
 
@@ -48,5 +73,7 @@ function autoClickerFunc() {
 }
 
 function infoAutoClicker() {
-	alert("you clicked info for auto clicker");
+	autoClickerNextLevel = autoClickerLevel + 1;
+	infoAutoClickerText = "Increase your score AUTOMAGICALLY!! Current Level " + autoClickerLevel + ", Next Level " + autoClickerNextLevel;
+	alert(infoAutoClickerText);
 }
